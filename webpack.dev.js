fc78fd2026bc5626ae1930481e3550e3aaea035c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-01-27 14:16:36 +0000

    Separate as dev and prod and use webpack-dev-server

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const common = require('./webpack.config')
+const {merge} = require('webpack-merge')
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+       port: 9000,
+    }
+})
